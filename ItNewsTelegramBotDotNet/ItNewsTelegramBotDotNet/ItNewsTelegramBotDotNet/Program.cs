d805543192c495b8878c513e075f28821857c70a
prostfk@gmail.com <prostfk@gmail.com>
2018-05-03 16:10:21 +0300

    init project

---
+++ ItNewsTelegramBotDotNet/ItNewsTelegramBotDotNet/ItNewsTelegramBotDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ItNewsTelegramBotDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
